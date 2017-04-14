7ccea62096ac7f468a6f3175f5efc8187f40e47d
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-04-14 10:31:54 -0500

    completed exercise.

---
+++ margaret.py
@@ -0,0 +1,60 @@
+import sys
+import pickle
+
+class Margaret:
+    """
+    Provide an interface for interacting with messages file.
+
+    Methods:
+    add_message,
+    serialize,
+    deserialize.
+    """
+    def __init__(self, message):
+        """
+        Arguments:
+        message, a string.
+        """
+        self.all_messages = {}
+        self.message = message
+        self.class_name = 'Margaret'
+        try:
+            self.all_messages = self.deserialize()
+        except FileNotFoundError:
+            pass
+
+    def add_message(self):
+        """
+        Add a new message to the all_messages dictionary.
+        """
+        try:
+            self.all_messages[self.class_name].append(self.message)
+        except KeyError:
+            self.all_messages[self.class_name] = [self.message]
+        print(self.all_messages)
+        self.serialize()
+
+    def serialize(self):
+        """
+        Write all_messages dictionary to messages file in a binary format.
+        """
+        with open('messages', 'wb+') as messages_file:
+            pickle.dump(self.all_messages, messages_file)
+
+    def deserialize(self):
+        """
+        Convert the messages file from binary.
+
+        Return the contents of the messages file.
+        """
+        try:
+            with open('messages', 'rb+') as messages_file:
+                self.all_messages = pickle.load(messages_file)
+        except EOFError:
+            pass
+
+        return self.all_messages
+
+if __name__ == '__main__':
+    margaret = Margaret(sys.argv[1])
+    margaret.add_message()
